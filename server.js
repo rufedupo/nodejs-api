402bffaa7be64f79f469cb0c64c8be09084ad2f2
horologii <ruanfelipedpontes@gmail.com>
2019-12-17 02:38:01 -0300

    CRUD de produtos adicionado

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 // Iniciando o App
 const app = express();
+app.use(express.json());
 
 // Iniciando o DB
 mongoose.connect(
@@ -14,14 +15,6 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  });
-});
+app.use('/', require('./src/routes'));
 
 app.listen(3000);
